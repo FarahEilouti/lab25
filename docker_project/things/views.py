2721d10380b047917c23dfd4e6cf40303e3b5f6e
Farah Riad Eilouti <fara7.eilouti@gmail.com>
2022-12-12 23:39:16 +0200

    incomplete

---
+++ docker_project/things/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.generics import ListAPIView,ListCreateAPIView,RetrieveAPIView,RetrieveUpdateAPIView,RetrieveUpdateDestroyAPIView
+from .serializers  import ThingSerializer
+# Create your views here.
+from .models import Thing
+
+
+class ThingListView(ListCreateAPIView):
+    queryset=Thing.objects.all()
+    serializer_class= ThingSerializer
+
+
+class ThingDetailView(RetrieveUpdateDestroyAPIView):
+    queryset=Thing.objects.all()
+    serializer_class= ThingSerializer
